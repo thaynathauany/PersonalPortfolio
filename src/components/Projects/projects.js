42c363f5f163e8e6e7f0c50375a5b9641163154a
Thayná Thauany <86171999+thaynathauany@users.noreply.github.com>
2023-09-17 20:09:50 -0300

    posts React adicionados

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,24 +1,32 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import projImg1 from "../../assets/img/project-img1.png"
 import { ProjectCardJS } from "../ProjectCard/projectCardJS";
 import { ProjectCardReact } from "../ProjectCard/projectCardReact";
 import colorSharp2 from "../../assets/img/color-sharp2.png"
-import capa1 from '../../assets/img-projects/1/capa.png'
-import capa2 from '../../assets/img-projects/2/capa.png'
+import projImg1 from "../../assets/img/project-img1.png"
+import organo from '../../assets/img-projects/1/capa.png'
+import space from '../../assets/img-projects/6/capa.png'
+import organic from '../../assets/img-projects/7/capa.png'
+import organo2 from '../../assets/img-projects/14/capa.png'
+import cineTag from '../../assets/img-projects/15/capa.png'
+import ttFlix from '../../assets/img-projects/16/capa.png'
+import imc from '../../assets/img-projects/17/capa.png'
+import jogoDaMemoria from '../../assets/img-projects/18/capa.png'
+import helloWorld from '../../assets/img-projects/19/capa.png'
+import loginForm from '../../assets/img-projects/20/capa.png'
 import capa3 from '../../assets/img-projects/3/capa.png'
-import capa4 from '../../assets/img-projects/4/capa.png'
+
 
 export const Projects = () => {
     const projectsJS = [
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: capa1,
+            imgUrl: capa3,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: capa2,
+            imgUrl: capa3,
         },
         {
             title: "Business Startup",
@@ -28,19 +36,64 @@ export const Projects = () => {
     ];
     const projectsReact = [
         {
-            title: "Teste React",
-            description: "Design & Development",
-            imgUrl: capa2,
+            title: "Organo",
+            description: "The website aims to organize teams, where respective roles can be assigned to each registered user.",
+            imgUrl: organo,
+            link: "https://organo-lilac-theta.vercel.app/"
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: capa2,
+            title: "Alura Space",
+            description: "To develop a gallery of images.",
+            imgUrl: space,
+            link: "https://alura-space-rho.vercel.app/"
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
+            title: "Organic Max",
+            description: "Learning how to create and manage global states with ContextAPI.",
+            imgUrl: organic,
+            link: "https://organic-max.vercel.app/"
+        },
+        {
+            title: "Organo 2",
+            description: "The website aims to organize teams, where respective roles can be assigned to each registered user and create a new team.",
+            imgUrl: organo2,
+            link: "https://organo2-eta.vercel.app/"
+        },
+        {
+            title: "Cine Tag",
+            description: "Building components and pages.",
+            imgUrl: cineTag,
+            link: "https://cine-tag-azure.vercel.app/"
+        },
+        {
+            title: "Tt Flix",
+            description: "Building a page using an API.",
+            imgUrl: ttFlix,
+            link: "https://tt-flix.vercel.app/"
+        },
+        {
+            title: "IMC",
+            description: "Building a page to enhance knowledge of useState in React.",
+            imgUrl: imc,
+            link: "https://projeto-imc-zeta.vercel.app/"
+        },
+        {
+            title: "Memory Game",
+            description: "Building a memory game.",
+            imgUrl: jogoDaMemoria,
+            link: "https://ola-mundo-two.vercel.app/posts/18"
+        },
+        {
+            title: "Hello world!",
+            description: "My first portfolio made in React.",
+            imgUrl: helloWorld,
+            link: "https://organo-lilac-theta.vercel.app/"
+        },
+        {
+            title: "Login Form",
+            description: "Junior React Developer Job Test",
+            imgUrl: loginForm,
+            link: "https://desafio-jr-react-5k8j.vercel.app/"
         },
     ];
 
@@ -60,7 +113,7 @@ export const Projects = () => {
                                     <Nav.Link eventKey="second">React</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="third">Programming Logic</Nav.Link>
+                                    <Nav.Link eventKey="third">HTML & CSS</Nav.Link>
                                 </Nav.Item>
                             </Nav>
                             <Tab.Content>
@@ -92,7 +145,7 @@ export const Projects = () => {
                                         }
                                     </Row>
                                 </Tab.Pane>
-                                <Tab.Pane eventKey="third">Lorem Ipsum</Tab.Pane>
+                                <Tab.Pane eventKey="third">HTML & CSS</Tab.Pane>
                             </Tab.Content>
                         </Tab.Container>
                     </Col>
